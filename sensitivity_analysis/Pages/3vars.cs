8567c076bf2bb885a9745a030b697cc80f68f401
jukraDD <justus.kranz@htw-dresden.de>
2023-11-27 17:17:45 +0100

    Added input functionality for 3 main variables

--- sensitivity_analysis/Pages/3vars.cs
+++ sensitivity_analysis/Pages/3vars.cs
@@ -12,10 +12,18 @@ namespace sensitivity_analysis.Pages
 {
     public partial class _3vars : Form
     {
+        double a, b, c = -1;
+        double ab111, ab011, ab101, ab001, ab110, ab010, ab100, ab000, ac111, ac011, ac101, ac001, ac110, ac010, ac100, ac000, bc111, bc011, bc101, bc001, bc110, bc010, bc100, bc000 = -1;
+        TextBox[] abhInputs;
+        Label[] unabhLabels;
+
         public _3vars()
         {
             InitializeComponent();
             tableProb.CellPaint += new TableLayoutCellPaintEventHandler(tableProb_CellPaint);
+            abhInputs = new[] { InputAabhB111, InputAabhB011, InputAabhB101, InputAabhB001, InputAabhB110, InputAabhB010, InputAabhB100, InputAabhB000, InputAabhC111, InputAabhC011, InputAabhC101, InputAabhC001, InputAabhC110, InputAabhC010, InputAabhC100, InputAabhC000, InputBabhC111, InputBabhC011, InputBabhC101, InputBabhC001, InputBabhC110, InputBabhC010, InputBabhC100, InputBabhC000 };
+            unabhLabels = new[] { LabelAunabh111, LabelAunabh011, LabelAunabh101, LabelAunabh001, LabelAunabh110, LabelAunabh010, LabelAunabh100, LabelAunabh000 };
+            LockAbhInput();
             relAB.SelectedIndex = 0;
             relABC.SelectedIndex = 0;
         }
@@ -85,6 +93,133 @@ namespace sensitivity_analysis.Pages
                 e.Graphics.DrawLine(pen, 143, 70, 110, 98);
                 e.Graphics.DrawLine(pen, 100, 126, 100, 154);
             }
+        }
+        private void LockAbhInput()
+        {
+            foreach (TextBox t in abhInputs)
+            {
+                t.Enabled = false;
+            }
+        }
+
+        private void UnlockAbhInput()
+        {
+            foreach (TextBox t in abhInputs)
+            {
+                t.Enabled = true;
+            }
+        }
+        private void ResetUnabhOutput()
+        {
+            foreach (Label l in unabhLabels)
+            {
+                l.Text = "";
+            }
+        }
+
+        private void ResetAbhInput()
+        {
+            foreach (TextBox t in abhInputs)
+            {
+                t.Text = "";
+            }
+        }
+
+        private bool IsValidInputABC(double a, double b, double c)
+        {
+            return !(a < 0 || a > 1 || b < 0 || b > 1 || c < 0 || c > 1);
+        }
+
+        private void GetValues_ABC(object sender)
+        {
+            ResetAbhInput();
+            if (sender == InputA)
+            {
+                if (!double.TryParse(InputA.Text.Replace(".", ","), out a) || a > 1 || a < 0)
+                {
+                    a = -1;
+                    if(InputA.Text != "") labelInputError1.Text = "Die Werte für P(A), P(B) und P(C) müssen mindestens 0 und maximal 1 sein.";
+                    else labelInputError1.Text = "";
+                    output_PnotA.Text = "";
+                    ResetUnabhOutput();
+                    LockAbhInput();
+                }
+                else
+                {
+                    output_PnotA.Text = (1 - a).ToString();
+                    int index = 0;
+                    foreach (Label l in unabhLabels)
+                    {
+                        if (index % 2 == 0) l.Text = a.ToString();
+                        else l.Text = (1 - a).ToString();
+                        index++;
+                    }
+                }
+            }
+            else if (sender == InputB)
+            {
+                if (!double.TryParse(InputB.Text.Replace(".", ","), out b) || b > 1 || b < 0)
+                {
+                    b = -1;
+                    if (InputB.Text != "") labelInputError1.Text = "Die Werte für P(A), P(B) und P(C) müssen mindestens 0 und maximal 1 sein.";
+                    else labelInputError1.Text = "";
+                    output_PnotB.Text = "";
+                    LockAbhInput();
+                }
+                else
+                {
+                    output_PnotB.Text = (1 - b).ToString();
+                }
+            }
+            else if (sender == InputC)
+            {
+                if (!double.TryParse(InputC.Text.Replace(".", ","), out c) || c > 1 || c < 0)
+                {
+                    c = -1;
+                    if (InputC.Text != "") labelInputError1.Text = "Die Werte für P(A), P(B) und P(C) müssen mindestens 0 und maximal 1 sein.";
+                    else labelInputError1.Text = "";
+                    output_PnotC.Text = "";
+                    LockAbhInput();
+                }
+                else
+                {
+                    output_PnotC.Text = (1 - c).ToString();
+                }
+            }
+            else
+            {
+                labelInputError1.Text = "Ungültige Eingabe";
+                LockAbhInput();
+            }
+
+            if (InputA.Text == "" && InputB.Text == "" && InputC.Text == "")
+            {
+                labelInputError1.Text = String.Empty;
+            }
+
+            if (InputA.Text != "" && InputB.Text != "" && InputC.Text != "" && IsValidInputABC(a,b,c))
+            {
+                labelInputError1.Text = String.Empty;
+                UnlockAbhInput();
+            }
+            else
+            {
+                LockAbhInput();
+            }
+        }
+        private void InputA_TextChanged(object sender, EventArgs e)
+        {
+            GetValues_ABC(sender);
+        }
+
+        private void InputB_TextChanged(object sender, EventArgs e)
+        {
+            GetValues_ABC(sender);
+        }
+
+        private void InputC_TextChanged(object sender, EventArgs e)
+        {
+            GetValues_ABC(sender);
         }
     }
 }
